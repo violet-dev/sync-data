2c875a8fddec026d9c1d259ce132ee677654d693
Violet <66541802+violet-dev@users.noreply.github.com>
2021-09-24 19:09:54 +0900

    Create sync-db.py

---
+++ sync-db.py
@@ -0,0 +1,123 @@
+# This source code is a part of project violet-server.
+# Copyright (C)2020-2021. violet-team. Licensed under the MIT Licence.
+
+import sys
+import os
+import os.path
+import time
+import shutil
+from subprocess import Popen, PIPE
+from datetime import datetime
+
+token = sys.argv[1]
+dbmetapath = 'syncversion.txt'
+
+def sync():
+  #
+  #   Sync (Low Performance Starting)
+  #
+  shutil.rmtree('chunk', ignore_errors=True)
+  process = Popen(['./hsync', '-ls', '--sync-only'])
+  process.wait()
+
+def release():
+  #
+  #   Create database
+  #
+  process = Popen(['./hsync', '-lc'])
+  process.wait()
+
+  #
+  #   Compress
+  #
+  process = Popen(['7z', 'a', 'rawdata.7z', 'rawdata/*'], stdout=open(os.devnull, 'wb'))
+  process.wait()
+  process = Popen(['7z', 'a', 'rawdata-chinese.7z', 'rawdata-chinese/*'], stdout=open(os.devnull, 'wb'))
+  process.wait()
+  process = Popen(['7z', 'a', 'rawdata-english.7z', 'rawdata-english/*'], stdout=open(os.devnull, 'wb'))
+  process.wait()
+  process = Popen(['7z', 'a', 'rawdata-japanese.7z', 'rawdata-japanese/*'], stdout=open(os.devnull, 'wb'))
+  process.wait()
+  process = Popen(['7z', 'a', 'rawdata-korean.7z', 'rawdata-korean/*'], stdout=open(os.devnull, 'wb'))
+  process.wait()
+
+  #
+  #   Rename rawdata/data.db
+  #
+  process = Popen(['mv', 'rawdata/data.db', 'rawdata/rawdata.db'])
+  process.wait()
+  process = Popen(['mv', 'rawdata-chinese/data.db', 'rawdata-chinese/rawdata-chinese.db'])
+  process.wait()
+  process = Popen(['mv', 'rawdata-english/data.db', 'rawdata-english/rawdata-english.db'])
+  process.wait()
+  process = Popen(['mv', 'rawdata-japanese/data.db', 'rawdata-japanese/rawdata-japanese.db'])
+  process.wait()
+  process = Popen(['mv', 'rawdata-korean/data.db', 'rawdata-korean/rawdata-korean.db'])
+  process.wait()
+
+  #
+  #   Upload
+  #
+  date = datetime.utcnow().strftime('%Y.%m.%d')
+  process = Popen(['github-release',
+    'upload', 
+    '--owner=violet-dev',
+    '--repo=db',
+    '--tag=' + date,
+    '--release-name=db ' + date + '',
+    '--body=""',
+    '--prerelease=false',
+    '--token=' + token,
+    'rawdata.7z',
+    'rawdata-chinese.7z',
+    'rawdata-english.7z',
+    'rawdata-japanese.7z',
+    'rawdata-korean.7z',
+    'rawdata/rawdata.db',
+    'rawdata-chinese/rawdata-chinese.db',
+    'rawdata-english/rawdata-english.db',
+    'rawdata-japanese/rawdata-japanese.db',
+    'rawdata-korean/rawdata-korean.db',
+  ])
+  process.wait()
+
+  timestamp = str(int(datetime.now().timestamp()))
+  url = 'https://github.com/violet-dev/db/releases/download/'+date+'/rawdata'
+  with open(dbmetapath, "a") as myfile:
+    myfile.write('db ' + timestamp + ' ' + url + '\n')
+
+def remove_exists(path):
+  if os.path.exists(path):
+    os.remove(path)
+
+def clean():
+  shutil.rmtree('rawdata', ignore_errors=True)
+  shutil.rmtree('rawdata-chinese', ignore_errors=True)
+  shutil.rmtree('rawdata-english', ignore_errors=True)
+  shutil.rmtree('rawdata-japanese', ignore_errors=True)
+  shutil.rmtree('rawdata-korean', ignore_errors=True)
+  
+  remove_exists('rawdata.7z')
+  remove_exists('rawdata-chinese.7z')
+  remove_exists('rawdata-english.7z')
+  remove_exists('rawdata-japanese.7z')
+  remove_exists('rawdata-korean.7z')
+
+def upload_db():
+  # donot use utcnow()
+  timestamp = str(int(datetime.now().timestamp()))
+
+  process = Popen(['github-release',
+    'upload', 
+    '--owner=violet-dev',
+    '--repo=sync-data',
+    '--tag=db_' + timestamp,
+    '--release-name=db_' + timestamp,
+    '--body=""',
+    '--prerelease=false',
+    '--token=' + token,
+    'data.db',
+  ])
+  process.wait()
+
+release()
