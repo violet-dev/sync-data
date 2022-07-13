b188c980019ee326e8dec696b68e5b670055db9d
Violet <66541802+violet-dev@users.noreply.github.com>
2022-07-13 11:03:46 +0900

    Update sync-chunk.py

--- sync-chunk.py
+++ sync-chunk.py
@@ -17,7 +17,7 @@ def sync():
   #   Sync (Low Performance Starting)
   #
   shutil.rmtree('chunk', ignore_errors=True)
-  process = Popen(['./hsync', '-ls', '--sync-only', '--exhentai-lookup-page', '20'])
+  process = Popen(['./hsync', '-ls', '--sync-only', '--sync-only-hitomi'])
   process.wait()
 
 def upload_chunk():
