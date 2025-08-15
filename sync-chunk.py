00c2e0be3a36ed373d5cc5ba7d2d8959a9124294
koromo <66541802+violet-dev@users.noreply.github.com>
2025-08-15 16:32:25 +0900

    Update sync-chunk.py

--- sync-chunk.py
+++ sync-chunk.py
@@ -17,7 +17,7 @@ def sync():
   #   Sync (Low Performance Starting)
   #
   shutil.rmtree('chunk', ignore_errors=True)
-  process = Popen(['./hsync', '-ls', '--sync-only', '--hitomi-sync-lookup-range', '30000', '--sync-only-hitomi'])
+  process = Popen(['./hsync', '-ls', '--sync-only', '--hitomi-sync-range', '3285404', '3483377', '--sync-only-hitomi'])
   process.wait()
 
 def upload_chunk():
